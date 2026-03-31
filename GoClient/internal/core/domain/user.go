1419ce9854b64fb4669d96176cd00b0b4838d5d8
monxvll <stedy3348112688@gmail.com>
2026-03-31 17:11:07 -0500

    feat(core): update user domain and create identity port for full profile auth

--- GoClient/internal/core/domain/user.go
+++ GoClient/internal/core/domain/user.go
@@ -1,9 +1,10 @@
 package domain
 
 type User struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Nickname string `json:"nickname"`
-	Email    string `json:"email"`
-	Gender   string `json:"gender"`
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Nickname  string `json:"nickname"`
+	Email     string `json:"email"`
+	Gender    string `json:"gender"`
 }
---
+++ GoClient/internal/core/ports/identity_provider.go
@@ -0,0 +1,27 @@
+package ports
+
+import (
+	"context"
+)
+
+// IdentityProvider defines the strict contract to work with WeiClothe.
+
+// ctx: All methods require a context as their first
+// parameter. This is used to manage request deadlines, cancellation signals,
+// and timeouts across the system to prevent memory leaks if the external 
+// identity provider becomes unresponsive.
+type IdentityProvider interface {
+
+	// Creates a new user record in the external IAM system.
+	// It takes the user's email and password, and returns the unique 
+	// user ID (uid) or an error if it fails.
+	RegisterUser(ctx context.Context, username, email, password, firstName, lastName string) (uid string, err error)
+
+	// Verifies the provided credentials against the IAM system.
+	// On successful authentication,  returns a JWT access token or an error
+	LoginUser(ctx context.Context, email, password string) (token string, err error)
+
+	// Receives a JWT string, verifies its signature and 
+	// expiration status, and returns the user ID (uid).
+	ValidateToken(ctx context.Context, token string) (uid string, err error)
+}

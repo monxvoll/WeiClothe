8bc5c0e80bcc4a14f8698fff68668b3a4ff22139
JoseSLK <jose.salamanca05@uptc.edu.co>
2026-04-03 13:35:22 -0500

    chore: update domain entitie user, add date birth

--- GoClient/internal/core/domain/user.go
+++ GoClient/internal/core/domain/user.go
@@ -1,10 +1,14 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Nickname  string `json:"nickname"`
-	Email     string `json:"email"`
-	Gender    string `json:"gender"`
+	ID          string    `json:"id"`
+	SubKeycloak string    `json:"sub_keycloak"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Nickname    string    `json:"nickname"`
+	Email       string    `json:"email"`
+	DateBirth   time.Time `json:"date_birth"`
+	Gender      string    `json:"gender"`
 }

359ce989906b34d548ae453de2d14d180c5499f3
monxvll <stedy3348112688@gmail.com>
2026-04-05 09:53:41 -0500

    feat(ports): implement new port for UpdateUser

--- GoClient/internal/core/ports/user_repository.go
+++ GoClient/internal/core/ports/user_repository.go
@@ -9,4 +9,7 @@ type UserRepository interface {
 	// Creates a new user record in the database.
 	// It takes a user object and returns an error if it fails.
 	CreateUser(ctx context.Context, user *domain.User) error
+
+	// Updates basic user information
+	UpdateUser(ctx context.Context, user *domain.User) error
 }

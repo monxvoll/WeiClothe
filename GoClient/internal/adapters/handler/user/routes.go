21582aecac9cbd3774a6e5aedc1d6857a8b88898
monxvll <stedy3348112688@gmail.com>
2026-04-08 15:50:16 -0500

    feat(user): expose POST /login route

--- GoClient/internal/adapters/handler/user/routes.go
+++ GoClient/internal/adapters/handler/user/routes.go
@@ -5,4 +5,6 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(g *gin.RouterGroup, h *HTTPHandler) {
 	g.GET("/ping", h.Ping)
 	g.POST("/register", h.Register)
+	g.POST("/login", h.Login)
+
 }
